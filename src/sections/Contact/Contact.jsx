4d77d87d9a0e48c62fa8fe19795c7666f3475c76
Ahsanullah Danish <ahsanullahdanish462@gmail.com>
2026-08-01 18:50:11 +0430

    Adding functionality to form.

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -1,45 +1,89 @@
-import React from 'react'
+
+import React, { useRef } from 'react'
+import emailjs from '@emailjs/browser'
 import styles from './contactStyle.module.css'
 
 function Contact() {
+  const form = useRef()
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs
+      .sendForm(
+        'service_ar1129x',
+        'template_rjocacq',
+        form.current,
+        {
+          publicKey: 'U94NfAivUhBFzaFi-',
+        }
+      )
+      .then(
+        () => {
+          alert('Message sent successfully! 🎉')
+          form.current.reset()
+        },
+        (error) => {
+          console.error('FAILED...', error)
+          alert('Something went wrong. Please try again.')
+        }
+      )
+  }
+
   return (
-    <section id='contact' className={styles.container}>
+    <section id="contact" className={styles.container}>
       <h1 className="sectionTitle">Contact</h1>
-      <form action="">
-        <div className='formGroup'>
+
+      <form ref={form} onSubmit={sendEmail}>
+        <div className="formGroup">
           <label htmlFor="name" hidden>
             Name
           </label>
-          <input type="text" name='name' id='name' 
-            placeholder='Name' required
+
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Name"
+            required
           />
         </div>
 
-        <div className='formGroup'>
+        <div className="formGroup">
           <label htmlFor="email" hidden>
             Email
           </label>
-          <input type="text" name='email' id='email' 
-            placeholder='Email' required
+
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email"
+            required
           />
         </div>
 
-        <div className='formGroup'>
+        <div className="formGroup">
           <label htmlFor="message" hidden>
             Message
           </label>
-          <textarea  name='message' id='message' 
-            placeholder='Message' required
+
+          <textarea
+            name="message"
+            id="message"
+            placeholder="Message"
+            required
           />
+
         </div>
 
-        <button>
+        <button type="submit">
           Submit
         </button>
-
       </form>
     </section>
   )
 }
 
 export default Contact
+
